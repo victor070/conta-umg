17de321e8965da34432e9031ccd5400ee51078bf
Victor Corado <jcorado@habitatguate.org>
2024-09-06 11:23:44 -0600

    app web v2.1.2

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -18,3 +18,22 @@ new Chart(ctx, {
         }
     }
 });
+
+const ctx2 = document.getElementById('myChart1');
+const labels = Utils.months({ count: 7 });
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+};
+new Chart(ctx2, {
+    type: 'line',
+    data: data,
+});
+
+
