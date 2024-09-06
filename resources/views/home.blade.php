5089cc8ff264546325a11c7eee2e7c2f81704a6a
Victor Corado <jcorado@habitatguate.org>
2024-09-06 09:55:41 -0600

    app web v2.14

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,5 +28,4 @@
 @endsection
 
 <!-- js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{asset('assets/js/home.js')}}"></script>
