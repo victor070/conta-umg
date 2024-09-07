f18c87848393337c42cab908f0d94e3344faf74e
Victor Corado <jcorado@habitatguate.org>
2024-09-07 16:00:13 -0600

    app web v3.1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
 
 Route::get('/', function () {
     return view('auth/login');
@@ -12,7 +11,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix'=>'clientes'],function(){
-	Route::get('','ClientesController@index');
+	Route::get('','App\Http\Controllers\ClientesController@index');
 	Route::get('{ids?}','ClientesController@confirm');
 	Route::post('import','ClientesController@import');
 	Route::post('search','ClientesController@search');
