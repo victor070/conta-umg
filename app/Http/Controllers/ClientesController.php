5ae25f98b728e4830d80f1b6670fb08b892ffc3d
Victor Corado <jcorado@habitatguate.org>
2024-09-07 19:31:09 -0600

    app web v3.1

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -50,7 +50,7 @@ class ClientesController extends Controller
             DB::rollback();
             return response()->json(array('error' => $e->errorInfo),404);
         }
-        return back()->withInput();
+        return redirect('/clientes');
 
     }
 
