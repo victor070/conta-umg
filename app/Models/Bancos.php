f034ea193fcb8cbc49a3e74d41cdc3879b1d47c8
Victor Corado <jcorado@habitatguate.org>
2024-09-09 17:29:27 -0600

    bancos v4.0

---
+++ app/Models/Bancos.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bancos extends Model
+{
+    protected $table = 'Banco';
+    protected $primaryKey = 'BancoID';
+
+    public $timestamps =false;
+
+    protected $fillable = [
+        'BancoID',
+        'Nombre',
+        'SaldosCuentas',
+        'Estatus'
+    ];
+}
